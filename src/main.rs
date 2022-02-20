1b6322743720c4ad5da507da05b048e88ddc5021
Nakayama Daiju <42.daiju@gmail.com>
2022-02-20 22:30:31 +0900

    time=0まで走らせる必要がある

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn gibbs_sampler(beta: f64, e0: f64, e1: f64) -> f64 {
     p1 / (p0 + p1)
 }
 
-pub struct ProppWilsonIsing {
+pub struct Ising {
     n2: usize,
     local_prob2: f64,
     local_prob4: f64,
@@ -16,15 +16,15 @@ pub struct ProppWilsonIsing {
     pointer: usize,
     /// Generated Random number
     /// lower 29-bits are index, higher 3-bits are cell-flip.
-    data: Vec<u32>,
+    generated_random_number: Vec<u32>,
     cell_up_start: Vec<u8>,
     cell_down_start: Vec<u8>,
 }
 
 const CELL_INDEX_BIT: u32 = 29;
 
-impl ProppWilsonIsing {
-    pub fn new(n: usize, beta: f64) -> ProppWilsonIsing {
+impl Ising {
+    pub fn new(n: usize, beta: f64) -> Ising {
         assert!(n * n < 2usize.pow(CELL_INDEX_BIT));
         let mut neighbors = Vec::with_capacity(n * n);
         for x in 0..n {
@@ -37,20 +37,25 @@ impl ProppWilsonIsing {
                 ]);
             }
         }
-        ProppWilsonIsing {
+        Ising {
             n2: n * n,
             local_prob2: gibbs_sampler(beta, -2.0, 2.0),
             local_prob4: gibbs_sampler(beta, -4.0, 4.0),
             neighbors,
             pointer: 0,
-            data: Vec::new(),
+            generated_random_number: Vec::new(),
             cell_up_start: vec![1; n * n],
             cell_down_start: vec![0; n * n],
         }
     }
 
-    pub fn rerun<R: Rng>(&mut self, n: usize, rng: &mut R) {
-        assert_eq!(n % self.n2, 0);
+    fn setup_next<R: Rng>(&mut self, rng: &mut R) {
+        let n = if self.generated_random_number.is_empty() {
+            1
+        } else {
+            // n *= 2;
+            self.generated_random_number.len()
+        };
         for _ in 0..n {
             let i = rng.gen_range(0..self.n2) as u32;
             let c = match rng.gen::<f64>() {
@@ -61,17 +66,16 @@ impl ProppWilsonIsing {
                 p if p < 1.0 - self.local_prob4 => 1,
                 _ => 0,
             };
-            self.data.push(i + (c << CELL_INDEX_BIT));
+            self.generated_random_number.push(i + (c << CELL_INDEX_BIT));
         }
-        self.pointer = self.data.len();
+        self.pointer = n;
         self.cell_up_start = vec![1; self.n2];
         self.cell_down_start = vec![0; self.n2];
     }
 
-    pub fn update_one(&mut self) {
-        debug_assert!(self.pointer > 0);
+    fn update_one(&mut self) {
         self.pointer -= 1;
-        let i = self.data[self.pointer];
+        let i = self.generated_random_number[self.pointer];
         let index = (i & ((1 << CELL_INDEX_BIT) - 1)) as usize;
         let threshold = (i >> CELL_INDEX_BIT) as u8;
         match threshold {
@@ -97,40 +101,55 @@ impl ProppWilsonIsing {
         self.cell_down_start[index] = if threshold <= m_down_start { 1 } else { 0 };
     }
 
-    pub fn update_n(&mut self) -> bool {
-        assert!(self.pointer >= self.n2);
-        for _ in 0..self.n2 {
+    pub fn rerun<R: Rng>(&mut self, rng: &mut R) -> bool {
+        self.setup_next(rng);
+        while self.pointer > 0 {
             self.update_one();
         }
-        self.pointer > 0
+        &self.cell_up_start == &self.cell_down_start
     }
 
-    pub fn distance(&self) -> u32 {
-        let mut s = 0;
-        for (&c0, &c1) in self.cell_up_start.iter().zip(self.cell_down_start.iter()) {
-            if c0 != c1 {
-                s += 1;
-            }
+    pub fn magnetization(&self) -> i64 {
+        let mut m = 0;
+        for &c in self.cell_up_start.iter() {
+            m += if c == 0 { -1 } else { 1 };
         }
-        s
+        m
+    }
+
+    pub fn energy(&self) -> i64 {
+        let mut e = 0;
+        for (&c, nei) in self.cell_up_start.iter().zip(self.neighbors.iter()) {
+            let mut local_m = 0;
+            for &j in nei {
+                local_m += self.cell_up_start[j];
+            }
+            e += (c as i64 * 2 - 1) * (local_m as i64 - 2);
+        }
+        e
+    }
+}
+
+fn run<R: Rng>(rng: &mut R, n: usize, temperature: f64) -> Option<(u8, i64, i64)> {
+    let mut ising = Ising::new(n, 1.0 / temperature);
+    let mut loop_count = 0;
+    let done = loop {
+        if loop_count == 30 {
+            break false;
+        }
+        if ising.rerun(rng) {
+            break true;
+        }
+        loop_count += 1;
+    };
+    if done {
+        Some((loop_count, ising.magnetization(), ising.energy()))
+    } else {
+        None
     }
 }
 
 fn main() {
     let mut rng = Mcg128Xsl64::new(1);
-    let mut ising = ProppWilsonIsing::new(20, 1.0 / 2.1);
-    let mut n = ising.n2;
-    'OUT: loop {
-        ising.rerun(n, &mut rng);
-        let mut i = 0;
-        while ising.update_n() {
-            i += 1;
-            let dist = ising.distance();
-            if dist == 0 {
-                println!("{}", i);
-                break 'OUT;
-            }
-        }
-        n *= 2;
-    }
+    println!("{:?}", run(&mut rng, 20, 3.0));
 }
